6a7fe780b01a1c1dfdc54a8880070c945517b7a9
calvached <calvache.diana@gmail.com>
2015-04-28 12:22:04 -0500

    Adding functionality for multiple threads

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -5,11 +5,17 @@ import main.java.server.router.Router;
 
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+
         HttpServer server =
-                new HttpServer(new ServerSocket(5000));
+                new HttpServer(
+                        new ServerSocket(5000),
+                        executorService);
 
         server.start();
     }
--- test/server/HttpServerTest.java
+++ test/server/HttpServerTest.java
@@ -1,7 +1,7 @@
 package server;
 
 import main.java.server.HttpServer;
-import mocks.MockBadServerSocket;
+import mocks.MockExecutorService;
 import mocks.MockServerSocket;
 
 import org.junit.Test;
@@ -10,36 +10,13 @@ import static org.junit.Assert.assertEquals;
 
 public class HttpServerTest {
     @Test
-    public void itRespondsToARequest() throws Exception {
+    public void itExecutesARunnable() throws Exception {
         MockServerSocket serverSocket = new MockServerSocket(3000);
+        MockExecutorService executorService = new MockExecutorService();
 
-        HttpServer server = new HttpServer(serverSocket);
+        HttpServer server = new HttpServer(serverSocket, executorService);
         server.start();
 
-        String outputResponse = serverSocket.getOutputResponse();
-
-        assertEquals("HTTP/1.1 200 OK\r\n" +
-                "\r\n" +
-                "<a href='/file1'>file1</a>\r\n" +
-                "<a href='/file2'>file2</a>\r\n" +
-                "<a href='/image.gif'>image.gif</a>\r\n" +
-                "<a href='/image.jpeg'>image.jpeg</a>\r\n" +
-                "<a href='/image.png'>image.png</a>\r\n" +
-                "<a href='/partial_content.txt'>partial_content.txt</a>\r\n" +
-                "<a href='/patch-content.txt'>patch-content.txt</a>\r\n" +
-                "<a href='/postData.txt'>postData.txt</a>\r\n" +
-                "<a href='/text-file.txt'>text-file.txt</a>\r\n", outputResponse);
-    }
-
-    @Test
-    public void itRespondsToABadRequest() throws Exception {
-        MockBadServerSocket serverSocket = new MockBadServerSocket(3000);
-
-        HttpServer server = new HttpServer(serverSocket);
-        server.start();
-
-        String outputResponse = serverSocket.getOutputResponse();
-
-        assertEquals("HTTP/1.1 400 Bad Request\r\n\r\n", outputResponse);
+        assertEquals(3, executorService.numOfExecutions);
     }
 }
